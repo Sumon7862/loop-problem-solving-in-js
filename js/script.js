12534ccb0f74f9813ba68a128cc35b2299b22f17
Sumon7862 <mdsumonmia383@gmail.com>
2025-12-28 23:19:47 +0600

    Add functions to print odd numbers, multiplication table of 5, and sum of numbers from 1 to 100

--- js/script.js
+++ js/script.js
@@ -21,3 +21,41 @@ function printEvenNumbers() {
 
     document.getElementById("evenResult").innerText = result;
 }
+
+// JavaScript function for prints odd numbers from 1 to 20.
+function printOddNumbers() {
+    let result = "";
+
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 !== 0) {
+            result += i + " ";
+        }
+    }
+
+    document.getElementById("oddResult").innerText = result;
+}
+
+// JavaScript function for prints multiplication table of 5.
+function printTableOfFive() {
+    let result = "";
+
+    for (let i = 1; i <= 10; i++) {
+        result += "5 × " + i + " = " + (5 * i) + "\n";
+    }
+
+    document.getElementById("tableResult").innerText = result;
+}
+
+// JavaScript function for sums numbers from 1 to 100.
+function sumOneToHundred() {
+    let sum = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        sum += i;
+    }
+
+    document.getElementById("sumResult").innerText = sum;
+}
+
+
+
